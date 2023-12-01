f0936387ffd0ec688617656c0e58e0a97f94edb2
Jyoti-0405 <jyoti@IN-8JMZ9K3.Nagarro.local>
2023-12-01 16:53:08 +0530

    docker and kubernetes files added

--- BackendApi/productCommunity/src/main/java/com/nagarro/ProductCommunityApplication.java
+++ BackendApi/productCommunity/src/main/java/com/nagarro/ProductCommunityApplication.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class ProductCommunityApplication {
+
+	@GetMapping("/")
+	public String welcome(){
+		return "Spring Boot Docker Demo";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCommunityApplication.class, args);
